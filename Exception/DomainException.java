0a276da23df45ffb571b6734e1c65500484f52c5
Marcos Santana <ssmarcosantana@gmail.com>
2022-08-30 20:24:28 -0300

    Good Solution Java Exception

---
+++ Exception/DomainException.java
@@ -0,0 +1,9 @@
+package Ex03.Exception;
+
+public class DomainException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public DomainException(String msg) {
+        super(msg);
+    }
+}
